0a165bd6f6fcb7c593b1952f1e74488b1bc3fc89
Harshksaw <mister.harshkumar@gmail.com>
2026-02-16 03:26:27 +0000

    feat: Implement Ragas evaluation for context precision and response relevancy, and add contextual compression to the retriever.

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -6,7 +6,8 @@ from  prod_assistant.utils.config_loader import load_config
 from  prod_assistant.utils.model_loader import ModelLoader
 from dotenv import load_dotenv
 
-
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainFilter
 
 class Retriever:
     def __init__(self):
@@ -49,12 +50,27 @@ class Retriever:
 
         if not self.retriever:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
-            self.retriever = self.vstore.as_retriever(
+            mmr_retriever = self.vstore.as_retriever(
                 search_type="mmr",
                 search_kwargs={"k": top_k, "fetch_k": 20,
                 "lambda_mult": 0.7,
                 "score_threshold": 0.3
                 })
+            print("MMR Retriever loaded")
+
+            llm = self.model_loader.load_llm()
+            print("LLM loaded")
+
+            compressor = LLMChainFilter.from_llm(llm)
+            print("Compressor loaded")
+
+            self.retriever = ContextualCompressionRetriever(
+                base_retriever=mmr_retriever,
+                base_compressor=compressor
+            )
+
+            return self.retriever
+
 
         return self.retriever
 
